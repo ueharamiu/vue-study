5d8726fcbd2c99b34c12bf0c620d2ad28022117d
ueharamiu <ueharamiu66@gmail.com>
2023-06-03 22:50:41 +0900

    test.vueをブラウザへ表示

---
+++ src/components/test.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>h1タグです</h1>
+		<p>テンプレート読み込みテスト</p>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import { createApp } from 'vue'
+import test from './components/test.vue'
+
+createApp( test ).mount( '#app' )
